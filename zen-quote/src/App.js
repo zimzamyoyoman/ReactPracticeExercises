45180b7e138106631653a8d1e95fb3d8d79eb681
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-10-12 12:36:24 +0530

    ZenQuote Generator

---
+++ zen-quote/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ZenQuote from './ZenQuote.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ZenQuote />
+    </div>
+  );
+}
+
+export default App;
---
+++ zen-quote/src/ZenQuote.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import './ZenQuote.css';
+
+class ZenQuote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { quote: "", isLoaded: false };
+    }
+
+    componentDidMount() {
+        axios.get("https://api.github.com/zen").then(response => {
+            setTimeout(
+                function () {
+                    this.setState({ quote: response.data, isLoaded: true });
+                }.bind(this),
+                3000
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.isLoaded ? (
+                    <div>
+                        <h1> Always remember...</h1>
+                        <p>{this.state.quote}</p>
+                    </div>)
+                    : (<div class="lds-ripple"><div></div><div></div></div>
+                    )}
+            </div>
+        )
+    }
+}
+
+export default ZenQuote;
